f38ac406e24d8abd4e0e755496906cae594dc41a
Rupesh6 <rupeshrockstar6666@gmail.com>
2022-08-02 14:27:34 +0530

    add login action to authenticate a user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resource :users, only: (:create)
+  resource :users, only: [:create]
+  post "/login", to: "users#login"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
